76163c02062b62f6d15df96780758bc2349bd3e9
RSPS Hobo <aminrahman24@gmail.com>
2018-07-11 21:32:13 +0000

    Stored texturs in an enum.

---
+++ src/main/java/io/battlerune/AnimatedTextureStore.java
@@ -0,0 +1,32 @@
+package io.battlerune;
+/*
+ * Holds the texturing Data.
+ */
+
+
+public enum AnimatedTextureStore {
+	
+		WATER_DROPLETS(17, 1),
+		WATER(24, 1),
+		MAGIC_TREE_STARS(34, 1),
+		LAVA(40, 1),
+		CRIMSON_LAVA(56, 1),
+		GRAY_LAVA(57, 1),
+		INFERNAL_LAVA(52, 2);
+		
+		private final int material_id;
+		private final int speed;
+		AnimatedTextureStore(int material_id, int speed) {
+			this.material_id = material_id;
+			this.speed = speed;
+		}
+		
+		public int getId() {
+			return material_id;
+		}
+		
+		public int getSpeed() {
+			return speed;
+		}
+		
+	}
